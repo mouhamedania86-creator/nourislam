98fd80687252c96b11b701c5858add7d2eed8f84
mouhamedania86-creator <mouhamedania86@gmail.com>
2026-06-30 23:55:25 +0100

    Update CompleteAdhkar.kt

--- app/src/main/java/com/example/CompleteAdhkar.kt
+++ app/src/main/java/com/example/CompleteAdhkar.kt
@@ -3,6 +3,7 @@ package com.example
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
