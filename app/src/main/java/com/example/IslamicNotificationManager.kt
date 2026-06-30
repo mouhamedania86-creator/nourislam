840ba362b7ee25912c1f30c522f9acd445be481d
mouhamedania86-creator <mouhamedania86@gmail.com>
2026-06-30 23:53:20 +0100

    Update IslamicNotificationManager.kt

--- app/src/main/java/com/example/IslamicNotificationManager.kt
+++ app/src/main/java/com/example/IslamicNotificationManager.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import com.example.R
 import java.util.Calendar
 
 object IslamicNotificationManager {
